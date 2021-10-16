f17a8c82e0a5440e118725c51a7e418fbcceb1b5
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-10-16 17:43:12 -0500

    inicializando estado y cargando imagens en el proyecto en /public

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Component } from 'react';
-import './App.css';
 
 class App extends Component {
+
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/assets/img/tomate.jpg' },
+      { name: 'Arbejas', price: 2500, img: '/assets/img/arbejas.jpg' },
+      { name: 'Lechuga', price: 1900, img: '/assets/img/lechuga.jpg' }
+    ]
+  };
+
   render() {
     return (
       <div>
