86deac0f401af86c0f2362fef11d4c449701d3e9
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-10-23 17:25:20 -0500

    Agregando y contabilizando productos al carrito

--- src/App.js
+++ src/App.js
@@ -10,10 +10,45 @@ class App extends Component {
       { name: 'Tomate', price: 1500, img: '/assets/img/tomate.jpg' },
       { name: 'Arbejas', price: 2500, img: '/assets/img/arbejas.jpg' },
       { name: 'Lechuga', price: 1900, img: '/assets/img/lechuga.jpg' }
+    ],
+    carro: [
     ]
   };
 
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    const existProd = carro.find(pro => pro.name === producto.name);
+
+    if (existProd) {
+      const newCarro = carro.map(pro => {
+        if (pro.name === producto.name) {
+          return {
+            ...pro,
+            cantidad: pro.cantidad + 1
+          };
+        } else {
+          return pro;
+        }
+      });
+
+      this.setState({
+        carro: newCarro
+      });
+
+    } else {
+      this.setState(
+        {
+          carro: this.state.carro.concat({
+            ...producto,
+            cantidad: 1
+          })
+        }
+      );
+    }
+  };
+
   render() {
+    console.log(this.state.carro)
     return (
       <div>
         <NavBar />
@@ -21,7 +56,7 @@ class App extends Component {
           <Titulo />
           <Productos
             productos={this.state.productos}
-            agregarAlCarro={() => console.log('click func prod')}
+            agregarAlCarro={this.agregarAlCarro}
           />
         </Layout>
       </div>
